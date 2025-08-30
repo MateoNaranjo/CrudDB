49fc340d76ee25b5a60c2c9c181dcaf9acf51c91
MateoNaranjo <mateo.naranjo.artunduaga@gmail.com>
2025-08-29 19:17:24 -0500

    print message

--- controllers/band_controller.py
+++ controllers/band_controller.py
@@ -3,11 +3,16 @@ from flask import Blueprint
 band_bp = Blueprint('band_bp', __name__)
 
 @app.route('/bands', methods=['GET'])
+    print("hola")
 
 @app.route('/bands/<int:band_id>', methods=['GET'])
+    print("hola")
 
 @app.route('/bands', methods=['POST'])
+    print("hola")
 
 @app.route('/bands/<int:band_id>', methods=['PUT'])
+    print("hola")
 
 @app.route('/bands/<int:band_id>', methods=['DELETE'])
+    print("hola")
